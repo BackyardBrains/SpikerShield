7f37bef54db8c7154b7227bf8f212416a8b732ca
stanislavmircic <stanislavmircic@gmail.com>
2017-10-06 13:58:25 +0200

    Update readSR.m

--- Muscle/Documentation/Matlab/readSR.m
+++ Muscle/Documentation/Matlab/readSR.m
@@ -1,7 +1,7 @@
 %Script reads about 10sec of data from BackyardBrain's Arduino SpikerShield
 %https://backyardbrains.com/products/heartAndBrainSpikerShieldBundle
 %script produces data in "result" variable
-s = serial('COM6');%change this to your com port
+s = serial('COM19');%change this to your com port
 set(s,'BaudRate',230400);
 s.InputBufferSize = 20000;
 s.Terminator ='';
@@ -23,27 +23,18 @@ end
 
 
 %unpacking data from frames
-foundBeginingOfFrame = 0;
 result = [];
-for i=1:2:length(data)-1
-    if(foundBeginingOfFrame==0)
-        %found begining of the frame
-        %frame begins with MSB set to 1
+i=1;
+while (i<length(data)-1)
         if(uint8(data(i))>127)
             foundBeginingOfFrame = 1;
             %extract one sample from 2 bytes
             intout = uint16(uint16(bitand(uint8(data(i)),127)).*128);
             i = i+1;
             intout = intout + uint16(uint8(data(i)));
-            result = [result intout];
+            result = [result intout];  
         end
-    else
-        %extract one sample from 2 bytes
-        intout = uint16(uint16(bitand(uint8(data(i)),127)).*128);
         i = i+1;
-        intout = intout +uint16( uint8(data(i)));
-        result = [result intout];
-    end
 end
 fclose(s);
 delete(s);
